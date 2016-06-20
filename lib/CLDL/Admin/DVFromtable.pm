45e57f635586929a657af1147dcc1b4d60b81561
jck000 <jck000@gmail.com>
2016-06-20 19:51:00 +0000

    modified:   DVFromtable.pm

--- lib/CLDL/Admin/DVFromtable.pm
+++ lib/CLDL/Admin/DVFromtable.pm
@@ -3,22 +3,51 @@ package CLDL::Admin::DVFromtable;
 use Dancer2 appname => 'CLDL';
 use Dancer2::Plugin::Database;
 
-use base qw( CLDL::Helpers::Defaults );
-use base qw( CLDL::Base );
+my $DV = { data_attributes => qq(buttonAlign: 'left', cache: true, cardView: false, contentType: 'json', iconPrefix: 'fa', icons:{ refresh: 'glyphicon-refresh icon-refresh', toggle: 'glyphicon-list-alt icon-list-alt', columns: 'glyphicon-th icon-th' }, maintainSelected: true, method: 'get', countColumns: 1, pagination: true, pageList: [ 50, 100, 250, 500], pageSize: 50, showHeader: true, showColumns: false, showRefresh: false, showToggle: false, sidePagination: 'client', singleSelect: false, smartDisplay: true, striped: true, search: true')};
+my $DVF = {
+            global => qq(
+                          'dvf_db_column' => '',
+                          'dvf_key'       => '0',
+                          'dvf_label'     => '',
+                          'dvf_name'      => '',
+                          'dvf_sortable'  => '0',
+                          'dvf_type'      => '1',
+                          'ordr'          => ''
+                        ),
+           active => { 
+                       dvf_type          => '5',
+                       dvf_values        => qq( { 0 => 'Inactive', 1 => 'Active' } ),
+                       dvf_default_value => '1'
+                     },
+           dv_type => { 
+                        dvf_type          => '5',
+                        dvf_values        => qq( { 0 => 'DataTable', 1 => 'Form' } ),
+                        dvf_default_value => '0'
+                      },
+           dt_del  => { 
+                        dvf_type          => '5',
+                        dvf_values        => qq( { 0 => 'No', 1 => 'Yes' } ),
+                        dvf_default_value => '1'
+                      },
+           dt_edit => { 
+                        dvf_type          => '5',
+                        dvf_values        => qq( { 0 => 'Not Editable', 1 => '1-Click', 2 => '2-Clicks' } ),
+                        dvf_default_value => '2'},
+          }; 
 
-my $DVF = $CLDL::Helpers::Defaults::DVF;
+use base qw( CLDL::Base );
 
 our $VERSION = '0.00001';
 
 prefix '/admin/dvfromtable';
 
 get '/select' => sub {
-  debug "IN fromtable";
+  debug "IN DVFromtable";
 
   my @tbl = CLDL::Base::tables;
 
   template 'cldl/admin/dvfromtable.tt', {
-                           'title'  => 'Create DataView from Table List ',
+                           title    => 'Create DataView from Table List ',
                            tbl_list => \@tbl,
                         };
 };
@@ -29,22 +58,25 @@ post '/insert' => sub {
   # DataView
   my $sth_idv = database->prepare( 
             'INSERT INTO cldl_dv 
-               ( company_id, dv_db_table, dv_name, dv_title, dv_data_attributes ) 
-                 VALUES 
-               ( ?, ?, ?, ?, ? )'
+                 ( company_id, dv_db_table, dv_name, dv_title, 
+                   dv_data_attributes ) 
+               VALUES 
+                 ( ?, ?, ?, ?, ? )'
   );
 
-  $sth_idv->execute( session('company_id'), 
+  $sth_idv->execute( 
+                     session('company_id'), 
                      params->{dv_db_table}, 
                      params->{dv_name}, 
                      params->{dv_title}, 
-                     $CLDL::Helpers::Defaults::DV);
+                     $DV->{data_attributes}
+                   );
 
   my $sth_last_id = database->prepare( 'SELECT LAST_INSERT_ID() AS id') ;
   $sth_last_id->execute();
   my $dv = $sth_last_id->fetchrow_hashref ;
 
-  my $schema = config->{Plugins}->{Database}->{database};
+  my $schema = config->{plugins}->{Database}->{database};
   my $working_table = params->{dv_db_table};
 
   debug "SCHEMA: $schema";
@@ -67,9 +99,9 @@ post '/insert' => sub {
   my $sth_idvf = database->prepare( 
             'INSERT INTO cldl_dvf 
                ( dv_id, dvf_db_column, dvf_key, dvf_label, dvf_name, ordr, 
-                 dvf_values, dvf_default_value )
+                 dvf_type, dvf_values, dvf_default_value )
                  VALUES 
-               ( ?, ?, ?, ?, ?, ?, ?, ? )'
+               ( ?, ?, ?, ?, ?, ?, ?, ?, ? )'
   );
   my $cnt = 0;
   foreach my $column ( @{$column_list} ) {
@@ -77,15 +109,33 @@ post '/insert' => sub {
     debug $column if $cnt == 0;
     $cnt++;
 
+    my $is_key = 1;
+    if ( $column->{mysql_is_pri_key} eq '' ) {
+      $is_key = 0;
+    }
+
+    #
+    # Beautify column names for use a labels:
+    #   1. Use the column name as a label
+    #   2. Change -(dash) and _(underscore) to space
+    #   3. Split based on space
+    #   4. Change each word to lower case but capitalize the 1st letter
+    #
+
+    my $dvf_label = $column->{COLUMN_NAME};
+    $dvf_label =~ s/[_-]/ /g; 
+    $dvf_label = join " ", map {ucfirst(lc)} split " ", $dvf_label;
+
     $sth_idvf->execute( $dv->{id}, 
                         $column->{COLUMN_NAME}, 
-                        $column->{mysql_is_pri_key}, 
-                        $column->{COLUMN_NAME},
+                        $is_key,
+                        $dvf_label,
                         $column->{COLUMN_NAME}, 
                         $column->{ORDINAL_POSITION},
-                        $DVF->{$column->{COLUMN_NAME}}->{dvf_values},
-                        $DVF->{$column->{COLUMN_NAME}}->{dvf_default_value}
-                        );
+                        $DVF->{ $column->{COLUMN_NAME} }->{dvf_type} || 0,
+                        $DVF->{ $column->{COLUMN_NAME} }->{dvf_values},
+                        $DVF->{ $column->{COLUMN_NAME} }->{dvf_default_value}
+                      );
   }
 
   redirect '/dv/select/' .  params->{dv_name};
