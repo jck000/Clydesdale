d8a1e241d43f37b89263105a1065dbf50c5e2bd2
jck000 <jck000@gmail.com>
2016-08-29 14:54:20 +0000

    Cleanup

--- lib/CLDL/DV.pm
+++ lib/CLDL/DV.pm
@@ -44,6 +44,8 @@ get '/select/:dv_name_id' => sub {
                           checkbox => 'true' } );
   }
 
+# [
+
 # from get_dvf  $dvf->{dvf_fields} = \@dvf_fields;
   foreach my $ref ( @{$dvf->{dvf_fields}} ) {
 
@@ -60,9 +62,6 @@ get '/select/:dv_name_id' => sub {
       my $dvf_field = { field => $ref->{dvf_db_column}, 
                         title => $ref->{dvf_label} };
 
-#      $qis_svryq->{fbegnoyr} = 'gehr' vs ($ers->{qis_fbegnoyr} == 1);
-
-      
       # Fixup JSON
       if (    defined $ref->{dvf_data_attributes} 
            && $ref->{dvf_data_attributes} ne '' ) {
@@ -81,7 +80,8 @@ get '/select/:dv_name_id' => sub {
   foreach my $ref ( @dvf_fields ) {
 
     # DB Column to select from DB
-    if ( $ref->{field} && $ref->{field} ne 'state') {
+    if ( $ref->{field} && (    $ref->{field} ne 'state'
+                            && $ref->{field} ne 'actionmenu' ) ) {
       push( @select, $ref->{field}) ;
     } elsif ( ! $ref->{field} && $ref->{dvf_db_column} ) {
       push( @select, $ref->{dvf_db_column}) ;
@@ -224,11 +224,31 @@ debug $data;
             || $cldl_dv_type == 2 
             || $cldl_dv_type == 3 ) {
 
+    my $ret_dvf_fields = undef;
+    if (    $dv->{dv_name} eq 'dv_list' 
+         || $dv->{dv_name} eq 'dvf_list' ) {
+      # $ret_dvf_fields = '[{"field":"actionmenu", "title":"Actions", "align":"center", "formatter":actionFormatter, "events":actionEvents}';
+      # $ret_dvf_fields = '[{"field":"actionmenu","title":"Actions","align":"center","formatter":actionFormatter}';
+      $ret_dvf_fields = '[';
+
+      my $cnt=0;
+      foreach my $dvf_entry ( @dvf_fields ) {
+        $ret_dvf_fields .= ',' if ( $cnt > 0 );
+        $ret_dvf_fields .= to_json( $dvf_entry );
+        $cnt++;
+      }
+      #$ret_dvf_fields .= ']';
+      $ret_dvf_fields .= ',{"field":"actionmenu","title":"Actions","align":"center","formatter":actionFormatter}]';
+    } else {
+      $ret_dvf_fields = to_json( \@dvf_fields ),
+    }
+
     # Datatable is always 'dv_dt'
     return template 'cldl/dv_dt.tt', { 
                                  cldl_dv_name_id => $cldl_dv_name,
                                  dv              => $dv, 
-                                 dvf             => to_json( \@dvf_fields ),
+#                                 dvf             => to_json( \@dvf_fields ),
+                                 dvf             => $ret_dvf_fields,
                                  cldl_menu       => session('cldl_menu'),
                                  data            => $data,
                                },
@@ -237,14 +257,31 @@ debug $data;
   }
 };
 
+post '/:action/:dv_name_id' => sub {
+  debug "In update-insert-delete ";
 
-post '/update/:dv_name_id' => sub {
-  debug "IN DV UPDATE";
+  debug "ACTION: " . params->{action};
+  debug "DV_ID:  " . params->{dv_name_id};
+
+  if ( params->{action} !~ 'update|insert|delete' ) {
+
+    debug "Return error";
+            status '404';
+            return "The page you are trying to reach does not exist. ";
+  }
+
+  &updates_to_db;
+
+};
+
+
+sub updates_to_db {
+  debug "IN DV UPDATES TO DB";
 
   my ($dv, $dvf, $data, $ref, @select, $key, $SQL, $sth_data, 
       @dvf_fields, @data_values, $sth_dv, $sth_dvf, 
-      @update_columns, @update_values, $key_column, $key_value, 
-      $sth_update);
+      @upd_db_columns, @upd_db_values, $key_column, $key_value, 
+      $sth_upd_db);
 
   debug "DV_NAME:" . params->{dv_name_id};
 
@@ -256,149 +293,98 @@ post '/update/:dv_name_id' => sub {
   ### DataView Fields
   $dvf = get_dvf( $dv->{dv_id} );
 
-  foreach my $ref ( @{$dvf->{dvf_fields}} ) {
-    # If it's a key, save it to add to the end of the column list
-    if ( $ref->{dvf_key} == 1 ) {
+  if ( params->{action} eq 'update' ) {
 
-      $key_column = $ref->{dvf_db_column} ;
-      $key_value  = params->{ $ref->{dvf_db_column} } ;
+    foreach my $ref ( @{$dvf->{dvf_fields}} ) {
+      #
+      # If it's a key, save it to add to the end of the column list
+      if ( $ref->{dvf_key} == 1 ) {
 
-    } elsif ( defined params->{ $ref->{dvf_db_column} } ) {
-
-      # If it's not a key column, add it to the column list
-      push( @update_columns, $ref->{dvf_db_column}) ;
-      push( @update_values,  params->{ $ref->{dvf_db_column} }) ;
-
+        $key_column = $ref->{dvf_db_column} ;
+        $key_value  = params->{ $ref->{dvf_db_column} } ;
+  
+      } elsif ( defined params->{ $ref->{dvf_db_column} } ) {
+  
+        # If it's not a key column, add it to the column list
+        push( @upd_db_columns, $ref->{dvf_db_column}) ;
+        push( @upd_db_values,  params->{ $ref->{dvf_db_column} }) ;
+  
+      }
     }
-  }
 
-  # Add column at the end
-  if ( defined $key_value && $key_value ne '' ) {
-    push( @update_values, $key_value ) ;
-  }
+    # Add column at the end
+    if ( defined $key_value && $key_value ne '' ) {
+      push( @upd_db_values, $key_value ) ;
+    }
 
-  $SQL = qq(UPDATE ) . $dv->{dv_db_table} 
-           . qq(\n SET ) . join(' = ?, ', @update_columns) . ' = ? '
-           . qq(\n   WHERE ) . $key_column . qq( = ? );
+    $SQL = qq(UPDATE ) . $dv->{dv_db_table} 
+             . qq(\n SET ) . join(' = ?, ', @upd_db_columns) . ' = ? '
+             . qq(\n   WHERE ) . $key_column . qq( = ? );
+
+  } elsif ( params->{action} eq 'insert' ) {
+
+    foreach my $ref ( @{$dvf->{dvf_fields}} ) {
+      if ( params->{ $ref->{dvf_db_column} } ) {
+        # add it to the column list
+        push( @upd_db_columns, $ref->{dvf_db_column}) ;
+        push( @upd_db_values,  params->{ $ref->{dvf_db_column} }) ;
+      }
+    }
+    $SQL = qq(INSERT INTO ) . $dv->{dv_db_table} 
+             . qq(\n    \( ) . join(', ', @upd_db_columns )         . qq( \) ) 
+             . qq(\n VALUES ) 
+             . qq(\n    \( ) . join(', ', ('?') x @upd_db_columns ) . qq( \) );
+    
+
+  } elsif ( params->{action} eq 'delete' ) {
+
+    foreach my $ref ( @dvf_fields ) {
+      # If it's a key, save it to add to the end of the column list
+      if ( $ref->{dvf_key} == 1 ) {
+    
+        $key_column = $ref->{dvf_db_column} ;
+        $key_value  = params->{ $ref->{dvf_db_column} } ;
+      }
+    }
+
+    # Add column at the end
+    if ( defined $key_value && $key_value ne '' ) {
+      push( @upd_db_values, $key_value ) ;
+    }
+
+    $SQL = qq(DELETE FROM ) . $dv->{dv_db_table} 
+             . qq(\n   WHERE ) . $key_column . qq( = ? );
+    
+
+  } 
 
 
   debug "SQL:";
   debug $SQL;
 
-  $sth_update = database->prepare( $SQL );
+  $sth_upd_db = database->prepare( $SQL );
 
   debug "Executing:";
-  debug @update_values;
+  debug @upd_db_values;
 
-  $sth_update->execute( @update_values );
+  $sth_upd_db->execute( @upd_db_values );
 
-  redirect config->{cldl}->{base_url} 
-             . '/dv/select/' 
-             .  $cldl_dv_name;
+  my $save_status = 0;  ## Success
+  if ( $sth_upd_db->err ) { 
+    $save_status = 1;
+  } 
+
+  my $dv_next = $dv->{dv_next} || session('cldl_return_to_page'); 
+
+  header( 'Content-Type'  => 'text/json' );
+  header( 'Cache-Control' =>  'no-store, no-cache, must-revalidate' );
+
+  to_json( { status         => $save_status,
+             cldl_next_page => $dv_next });
+
 };
 
 
-post '/insert/:dv_name_id' => sub {
-  debug "IN DV UPDATE";
-
-  my ($dv, $dvf, $data, $key, $SQL, 
-      @dvf_fields, @data_values, $sth_dv, $sth_dvf, 
-      @insert_columns, @insert_values, $key_column, $key_value, 
-      $sth_insert);
-
-  debug "DV_NAME:" . params->{dv_name_id};
-
-  my $cldl_dv_name    = params->{dv_name_id} ||= params->{dv_name_override} ;
-
-  ### DataView
-  $dv = get_dv( $cldl_dv_name, session('company_id') );
-
-  ### DataView Fields
-  $dvf = get_dvf( $dv->{dv_id} );
-
-  foreach my $ref ( @{$dvf->{dvf_fields}} ) {
-    if ( params->{ $ref->{dvf_db_column} } ) {
-      # add it to the column list
-      push( @insert_columns, $ref->{dvf_db_column}) ;
-      push( @insert_values,  params->{ $ref->{dvf_db_column} }) ;
-    }
-  }
-
-  $SQL = qq(INSERT INTO ) . $dv->{dv_db_table} 
-           . qq(\n    \( ) . join(', ', @insert_columns )         . qq( \) ) 
-           . qq(\n VALUES ) 
-           . qq(\n    \( ) . join(', ', ('?') x @insert_columns ) . qq( \) );
-
-
-  debug "SQL:";
-  debug $SQL;
-
-  $sth_insert = database->prepare( $SQL );
-
-  debug "Executing:";
-  debug @insert_values;
-
-  $sth_insert->execute( @insert_values );
-
-  redirect config->{cldl}->{base_url} 
-             . '/dv/select/' 
-             .  $cldl_dv_name;
-};
-
-
-
-### NOT DONE AT ALL
-post '/delete/:dv_name_id' => sub {
-  debug "IN DV DELETE";
-
-  my ($dv, $dvf, $data, $ref, @select, $key, $SQL, $sth_data, 
-      @dvf_fields, @data_values, $sth_dv, $sth_dvf, 
-      @delete_columns, @delete_values, $key_column, $key_value, 
-      $sth_delete);
-
-  debug "DV_NAME:" . params->{dv_name_id};
-
-  my $cldl_dv_name    = params->{dv_name_id} ||= params->{dv_name_override} ;
-
-  foreach my $ref ( @dvf_fields ) {
-    # If it's a key, save it to add to the end of the column list
-    if ( $ref->{dvf_key} == 1 ) {
-
-      $key_column = $ref->{dvf_db_column} ;
-      $key_value  = params->{ $ref->{dvf_db_column} } ;
-
-    } else {
-
-    # If it's just a column, add it to the column list
-      push( @delete_columns, $ref->{dvf_db_column}) ;
-      push( @delete_values,  params->{ $ref->{dvf_db_column} }) ;
-
-    }
-  }
-
-  # Add column at the end
-  if ( defined $key_value && $key_value ne '' ) {
-    push( @delete_values, $key_value ) ;
-  }
-
-  $SQL = qq(DELETE FROM ) . $dv->{dv_db_table} 
-           . qq(\n   WHERE ) . $key_column . qq( = ? );
-
-
-  debug "SQL:";
-  debug $SQL;
-
-  $sth_delete = database->prepare( $SQL );
-
-  debug "Executing:";
-  debug @delete_values;
-
-  $sth_delete->execute( @delete_values );
-
-  redirect config->{cldl}->{base_url} 
-             . '/dv/select/' 
-             .  $cldl_dv_name;
-};
 
 ### Maybe?
 sub return_dv_dt {
@@ -432,12 +418,14 @@ sub get_dv {
                     dv_update_sql,
                     dv_template,
                     dv_js_functions,
+                    dv_next,
                     dv_data_attributes
                FROM cldl_dv
-                 WHERE dv_name    = ? 
+                 WHERE     dv_name = ? 
+                       AND active  = 1 
                        AND (    company_id = ? 
-                             OR company_id = 1 )
-                       AND active = 1 )
+                             OR company_id = 1 ) 
+           )
   );
 
   $sth_dv->execute( $dv_name_id, $company_id );
@@ -505,255 +493,3 @@ sub get_dvf {
 1;
 
 
-### # Return an array of table names
-### sub tables {
-###     my @tbl;
-### 
-###     my @table_list = database->tables('', '', '', 'TABLE');
-### 
-###     for my $table ( @table_list) {
-###       $table =~ s/`//g;
-###       my @tmp_tbl = split( /\./, $table);
-###       push @tbl, $tmp_tbl[1];
-###     }
-### 
-###     return @tbl;
-### }
-### 
-### sub selectedvalue {
-###   my $ref        = shift;
-###   my $dvf_d_id   = shift;
-###   my $company_id = shift;
-### 
-###   my $sth_df_dt  = database->prepare( $SQL_LIB->{DF_DT}  );
-### 
-###   # loop through each field and see if there's a lookup value for it
-###   #   if there is, then get it's value from the DB
-###   foreach my $skey ( keys %{$ref} ) {
-### 
-###     # Grab unique keys for lookup
-###     if ( defined $dvf_d_id->{ $skey } ) {
-### 
-###       $sth_df_dt->execute( $dvf_d_id->{ $skey }, 
-###                            $company_id, 
-###                            $ref->{ $skey } );
-### 
-###       my $tmp_ref = $sth_df_dt->fetchrow_hashref ;
-###       $ref->{ $skey } = $tmp_ref->{df_label};
-###     }
-###   }
-### 
-###   return $ref;
-### }
-### 
-
-### package CLDL::Base;
-### 
-### use Dancer ':syntax';
-### use Dancer::Plugin::Database;
-### 
-### our $VERSION = '0.00001';
-### 
-### 
-### # SQL statements
-### my $SQL_LIB = {
-###      DV => 
-###          qq( SELECT dv_id,
-###                     dv_name,
-###                     dv_db_table,
-###                     dv_type,
-###                     dv_title,
-###                     dt_add,
-###                     dt_del,
-###                     dt_edit, 
-###                     dv_name_add, 
-###                     dv_name_edit, 
-###                     dv_template,
-###                     dv_js_functions,
-###                     dv_data_attributes
-###                FROM cldl_dv
-###                  WHERE dv_name    = ? 
-###                        AND (    company_id = ? 
-###                              OR company_id = 1 )
-###                        AND active = 1 ),
-###      DVF => 
-###          qq( SELECT dvf_id, 
-###                     ordr,
-###                     dvf_db_column,
-###                     dvf_name,
-###                     dvf_label, 
-###                     dvf_type,
-###                     dvf_key,
-###                     dvf_placeholder,
-###                     dvf_help,
-###                     dvf_sortable,
-###                     dvf_sort_ordr,
-###                     dvf_sort_asc_desc,
-###                     d_id,
-###                     dvf_js_functions,
-###                     dvf_data_attributes
-###                FROM cldl_dvf 
-###                  WHERE dv_id      = ?
-###                        AND active = 1 
-###                    ORDER BY ordr  ),
-###      D => 
-###          qq( SELECT d_id, 
-###                     d_name
-###                FROM cldl_d
-###                  WHERE d_id           = ? 
-###                        AND (    company_id = ? 
-###                              OR company_id = 1 )
-###                        AND active     = 1 ),
-###      DF => 
-###          qq( SELECT df_id, 
-###                     ordr,
-###                     df_label,
-###                     df_value,
-###                     df_default
-###                FROM cldl_df 
-###                  WHERE d_id       = ?
-###                        AND active = 1 
-###                    ORDER BY ordr  ),
-###      DF_DT => 
-###          qq( SELECT df_label
-###                FROM cldl_d d,
-###                     cldl_df df
-###                  WHERE d.d_id           = ? 
-###                        AND (    d.company_id = ? 
-###                              OR d.company_id = 1 )
-###                        AND d.active     = 1 
-### 
-###                        AND df.df_value  = ?
-###                        AND df.d_id      = d.d_id 
-###                        AND df.active    = 1  ),
-###      DF_DT =>
-###          qq( SELECT df_label
-###                FROM cldl_d d,
-###                     cldl_df df
-###                  WHERE d.d_id           = ? 
-###                        AND (    d.company_id = ? 
-###                              OR d.company_id = 1 )
-###                        AND d.active     = 1 
-### 
-###                        AND df.df_value  = ?
-###                        AND df.d_id      = d.d_id 
-###                        AND df.active    = 1  ),
-###      MENU => qq( 
-###                 SELECT menu_id,
-###                        ordr,
-###                        menu_label,
-###                        menu_link,
-###                        menu_js_functions,
-###                        menu_data_attributes,
-###                        menu_notes
-###                   FROM cldl_menu
-###                     WHERE (    company_id = ?
-###                             OR company_id = 1 )
-###                           AND pmenu_id IS NULL
-###                           AND active   = 1
-###                   UNION
-###                 SELECT pmenu_id AS menu_id,
-###                        ordr,
-###                        menu_label,
-###                        menu_link,
-###                        menu_js_functions,
-###                        menu_data_attributes,
-###                        menu_notes
-###                   FROM cldl_menu
-###                     WHERE (    company_id = ?
-###                             OR company_id = 1 )
-###                           AND pmenu_id IS NOT NULL
-###                           AND active   = 1
-###                    ORDER BY menu_id, ordr ),
-### };
-### 
-### # Return an array of table names
-### sub tables {
-###     my @tbl;
-### 
-###     my @table_list = database->tables('', '', '', 'TABLE');
-### 
-###     for my $table ( @table_list) {
-###       $table =~ s/`//g;
-###       my @tmp_tbl = split( /\./, $table);
-###       push @tbl, $tmp_tbl[1];
-###     }
-### 
-###     return @tbl;
-### }
-### 
-### sub formdatalists {
-###   my $dvf_d_id   = shift;
-###   my $company_id = shift;
-### 
-###   my $d = {};
-### 
-###   # DataView Field Values ( checkbox, radio, select)
-###   my $sth_d  = database->prepare( $SQL_LIB->{D}  );
-###   my $sth_df = database->prepare( $SQL_LIB->{DF} );
-### 
-###   foreach my $skey ( keys %{$dvf_d_id} ) { # skey   = field name (s=search)
-###     my $s_d_id = $dvf_d_id->{ $skey };     # s_d_id = key to data(s=search)
-###     $sth_d->execute( $s_d_id, $company_id );
-###     $d->{ $s_d_id } = $sth_d->fetchrow_hashref ;
-### 
-###     my @d_fields = ();
-###     $sth_df->execute( $s_d_id );
-###     while ( my $d_ref = $sth_df->fetchrow_hashref ) {
-###       push( @d_fields, $d_ref);
-###     }
-###     $d->{ $s_d_id }->{dtl} = \@d_fields;
-###   }
-### 
-###   return $d;
-### }
-### 
-### sub selectedvalue {
-###   my $ref        = shift;
-###   my $dvf_d_id   = shift;
-###   my $company_id = shift;
-### 
-###   my $sth_df_dt  = database->prepare( $SQL_LIB->{DF_DT}  );
-### 
-###   # loop through each field and see if there's a lookup value for it
-###   #   if there is, then get it's value from the DB
-###   foreach my $skey ( keys %{$ref} ) {
-### 
-###     # Grab unique keys for lookup
-###     if ( defined $dvf_d_id->{ $skey } ) {
-### 
-###       $sth_df_dt->execute( $dvf_d_id->{ $skey }, 
-###                            $company_id, 
-###                            $ref->{ $skey } );
-### 
-###       my $tmp_ref = $sth_df_dt->fetchrow_hashref ;
-###       $ref->{ $skey } = $tmp_ref->{df_label};
-###     }
-###   }
-### 
-###   return $ref;
-### }
-### 
-
-
-### 
-### sub menu {
-###   my $company_id = shift;
-###   my $menu;
-### 
-###   my $sth_menu = database->prepare( $SQL_LIB->{MENU} );
-### 
-###   $sth_menu->execute( $company_id, $company_id );
-### 
-###   while ( my $ref = $sth_menu->fetchrow_hashref ) {
-###     push( @{$menu}, $ref);
-###   }  
-### 
-###   return $menu;
-### 
-### }
-### 
-### 
-### 
-### 1;
-### 
