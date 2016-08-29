e6196c7e56599a5454efae83597ef38aa227d397
jck000 <jck000@gmail.com>
2016-08-29 15:04:44 +0000

    use app.pl instead 	deleted:    bin/app.psgi

--- bin/launch_dev.sh
+++ bin/launch_dev.sh
@@ -2,4 +2,5 @@
 # Dev
 #
 
-plackup -E CLDL -p 30056 -R lib,views ./bin/app.psgi
+nohup plackup -E CLDL -p 30056 -R lib,views ./bin/app.pl > /home/fasv/apps/CLDL/logs/launch.log & 
+#nohup plackup -E CLDL -p 30056 -R lib,views ./bin/app.psgi > /home/fasv/apps/CLDL/logs/launch.log & 
