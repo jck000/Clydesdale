bb117c2212206016e96b33b95de630e811445721
jck000 <jck000@gmail.com>
2016-10-15 18:53:22 +0000

    Simple script for dev Added a new module to speed up Dancer Cleanup
    Change paths to relative paths to take advantage of <base href=

--- bin/launch_dev.sh
+++ bin/launch_dev.sh
@@ -2,5 +2,4 @@
 # Dev
 #
 
-nohup plackup -E CLDL -p 30056 -R lib,views ./bin/app.pl > /home/fasv/apps/CLDL/logs/launch.log & 
-#nohup plackup -E CLDL -p 30056 -R lib,views ./bin/app.psgi > /home/fasv/apps/CLDL/logs/launch.log & 
+plackup -E CLDL -R lib,views ./bin/app.pl 
