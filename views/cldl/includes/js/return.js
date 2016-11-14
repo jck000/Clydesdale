a2fe164ad2adc04184797202463366c3de8286de
jck000 <jck000@gmail.com>
2016-11-14 12:30:27 +0000

    Changed paths/cleanup

--- views/cldl/includes/js/return.js
+++ views/cldl/includes/js/return.js
@@ -1,5 +1,6 @@
 
       $('.returnbutton').click( function() {
-        window.location.assign("[% session.cldl_return_to_page %]");
+//        window.location.assign("[% session.cldl_return_to_page %]");
+        window.location.assign("[% cldl_return_to_page %]");
       });
 
