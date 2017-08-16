f629944516bbcc1e752e026cc85b276c131eaec8
jck000 <jck000@gmail.com>
2017-08-16 04:04:01 +0000

    javascript changes

--- views/cldl/includes/js/reload.js
+++ views/cldl/includes/js/reload.js
@@ -1,6 +1,7 @@
 
       $('.resetbutton').click( function() {
+        window.location.reload(true);
 //        window.location.assign("[% session.cldl_reload_page %]");
-        window.location.assign("[% cldl_reload_page %]");
+//        window.location.assign("[% cldl_reload_page %]");
       });
 
